bf56dd53743e54dac7f8c3fbb8f87cf6862f38e0
AlexeyIlchenko <alexeyi@wizardsdev.com>
2019-09-17 15:30:32 +0300

    create simple example

--- src/Screen/Screen.js
+++ src/Screen/Screen.js
@@ -1,13 +1,65 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, TouchableWithoutFeedback } from 'react-native';
 
 import { styles } from './styles';
 
-export default class OpenGL extends Component {
+export default class Screen extends Component {
+
+    constructor(props) {
+        super(props);
+        this.websocket = null;
+        this.wsUrl = 'wss://informer.finversia.ru/wss/Server.ashx?subscriber=true/';
+        this.state = {
+            textConnection: '',
+        }
+    }
+
+
+    componentDidMount = async () => {
+        try {
+            this.websocket = await new WebSocket(this.wsUrl);
+            console.log('this.websocket', this.websocket);
+
+            this.websocket.onopen = () => {
+                this.setState({ textConnection: 'connected to WS' })
+            }
+
+        } catch (error) {
+
+        }
+
+    }
+
+    onMessageWs = () => {
+        this.websocket.onmessage = (e) => {
+            const data = JSON.parse(e.data);
+            console.log(data)
+        }
+
+        this.setState({ textConnection: 'recieveing data from server' })
+    }
+
+    onCloseConnection = () => {
+        this.websocket.close();
+        this.websocket.onclose = (e) => {
+            console.log("WebSocket is closed now.", this.websocket);
+        };
+        this.setState({ textConnection: 'connection is closed' })
+    }
+
     render() {
+        const { textConnection } = this.state;
+
         return (
             <View style={styles.container}>
-                <Text>Screen</Text>
+                <Text>Status: {textConnection}</Text>
+                <Text>------------------------------</Text>
+                <TouchableWithoutFeedback onPress={this.onMessageWs}>
+                    <Text>start resieve data from server</Text>
+                </TouchableWithoutFeedback>
+                <TouchableWithoutFeedback onPress={this.onCloseConnection}>
+                    <Text>close connection</Text>
+                </TouchableWithoutFeedback>
             </View>
         );
     }
